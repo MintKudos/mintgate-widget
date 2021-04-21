79762b6c064dfb7c79cb64c8403fec599543d387
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2021-04-20 23:40:19 -0400

    scaffolding that works

--- widget-component-library/src/index.js
+++ widget-component-library/src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import {render} from "react-dom";
 import Example from "./lib/components/example"
-import TPPFormPanel from "./lib/components/TPPFormPanel";
 
 const App = () => (
     <div>
         <Example></Example>
-        <TPPFormPanel></TPPFormPanel>
     </div>
 );
 
