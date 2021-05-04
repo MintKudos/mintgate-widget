d9931299e6d507bb99db1b248afa4ef5b8de9f3e
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2021-05-04 12:21:21 -0400

    added babel

--- widget-component-library/src/lib.js
+++
@@ -1,5 +0,0 @@
-import TPPFormWidget from "./lib/components/token-gating-form/TPPFormWidget"
-import './index.css';
-import "tailwindcss/tailwind.css"
-
-export { TPPFormWidget };
---
+++ widget-component-library/src/lib/index.js
@@ -0,0 +1,4 @@
+import TPPFormWidget from "./components/token-gating-form/TPPFormWidget"
+import "tailwindcss/tailwind.css"
+
+export { TPPFormWidget };
