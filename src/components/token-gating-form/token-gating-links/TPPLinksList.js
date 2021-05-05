9edb2a79cce4c2ff5d3a2f8a30173ea6c5e39afb
Simon Weniger <hello@simonweniger.com>
2021-05-05 13:06:39 +0200

    added design from old PR

---
+++ src/components/token-gating-form/token-gating-links/TPPLinksList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import TPPCardWideEmbed from "./TPPCardWideEmbed"
+
+function TPPLinksList() {  
+
+  return(
+    <div data-theme="light">  {/* 
+      17 Themes are available: 
+      - aqua
+      - black
+      - bumblebee
+      - cupcake
+      - cyberpunk
+      - dark
+      - dracula
+      - fantasy
+      - forest
+      - garden
+      - halloween
+      - light (default)
+      - luxury
+      - pastel
+      - retro
+      - synthwave
+      - valentine  */}
+      <div className="space-y-4">
+      <TPPCardWideEmbed />
+      <TPPCardWideEmbed />
+      <TPPCardWideEmbed />
+      <TPPCardWideEmbed />
+      </div>
+      </div>
+  );
+}
+
+export default TPPLinksList 
