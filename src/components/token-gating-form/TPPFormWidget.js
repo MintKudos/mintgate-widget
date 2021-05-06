5bbfc5554e0788c4b32615aaac91f464d4df0ae4
Simon Weniger <hello@simonweniger.com>
2021-05-06 14:33:42 +0200

    minor link input style change

--- src/components/token-gating-form/TPPFormWidget.js
+++ src/components/token-gating-form/TPPFormWidget.js
@@ -166,9 +166,9 @@ function TPPFormWidget(props, preselect, onClose) {
             id="form_url"
             name="contentURL"
             type="text" 
-            placeholder="Paste the link you want to token gate" className={`w-full pr-16 input shadow-md focus:ring-primary focus:ring-4 label-text text-sm font-heading font-semibold ${nextStepOpen ? 'input-gohst shadow-none' : 'ring-4 ring-primary ring-opacity-20'}`} /> 
+            placeholder="Paste the link you want to token gate" className={`w-full py-4 px-6 input shadow-md focus:ring-primary focus:ring-4 label-text text-sm font-heading font-semibold ${nextStepOpen ? 'input-gohst shadow-none' : 'ring-4 ring-primary ring-opacity-20'}`} /> 
             <span onClick={() => {
-              setNextStepOpen(true);}} className={`absolute right-0 rounded-l-none btn btn-primary hover:btn-secondary ${nextStepOpen ? 'hidden' : ''}`}>next</span>
+              setNextStepOpen(true);}} className={`absolute right-0 rounded-lg btn btn-primary hover:btn-secondary ${nextStepOpen ? 'hidden' : ''}`}>next</span>
           </div>
         </div> 
         <Transition
